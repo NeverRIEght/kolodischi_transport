af9bc5a5d33c20c6a90539f0edafc919312cf776
NeverRIEght <lfkomarovme@gmail.com>
2023-07-11 12:52:51 +0300

    Add HTMLUnit with code example

--- src/main/java/dev/neverrieght/kolodischi_transport/MainApplication.java
+++ src/main/java/dev/neverrieght/kolodischi_transport/MainApplication.java
@@ -5,7 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import com.gargoylesoftware.htmlunit.*;
+import com.gargoylesoftware.htmlunit.html.*;
+import org.w3c.dom.html.HTMLLinkElement;
+
 import java.io.IOException;
+import java.util.*;
 
 public class MainApplication extends Application {
     @Override
@@ -14,7 +19,42 @@ public class MainApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 400, 600);
         stage.setTitle("Kolodischi Transport");
         stage.setScene(scene);
+
+        WebClient webClient = new WebClient(BrowserVersion.CHROME);
+
+        webClient.getOptions().setCssEnabled(false);
+        webClient.getOptions().setJavaScriptEnabled(false);
+        String webPageURL = "https://kolodischi.by/raspisanie-avtobusov/2765-mihailov-kut-na-minsk";
+
+
+        try {
+
+            HtmlPage page = webClient.getPage(webPageURL);
+
+            List<HtmlTableDataCell> timesList = page.getByXPath("/html/body/div[1]/div[2]/div/div[1]/div/div/div[1]/div/div[1]/table/tbody/tr[3]/td |" +
+                    "/html/body/div[1]/div[2]/div/div[1]/div/div/div[1]/div/div[1]/table/tbody/tr[4]/td |" +
+                    "/html/body/div[1]/div[2]/div/div[1]/div/div/div[1]/div/div[1]/table/tbody/tr[5]/td |" +
+                    "/html/body/div[1]/div[2]/div/div[1]/div/div/div[1]/div/div[1]/table/tbody/tr[6]/td");
+
+            List<HtmlTableDataCell> routeNumber = page.getByXPath("/html/body/div[1]/div[2]/div/div[1]/div/div/div[1]/div/div[1]/table/tbody/tr[1]/td");
+
+            System.out.println(routeNumber.get(0).getTextContent().trim());
+
+            for (HtmlTableDataCell td : timesList) {
+                System.out.print(td.getTextContent().trim());
+            }
+//            /html/body/div[1]/div[2]/div/div[1]/div/div/div[1]/div/ul/li[1]/a/i
+//            System.out.println(page.getTitleText());
+
+        } catch (FailingHttpStatusCodeException | IOException e) {
+            e.printStackTrace();
+        }
+
+
         stage.show();
+
+
+
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,9 @@ module dev.neverrieght.kolodischi_transport {
     requires javafx.fxml;
             
         requires org.controlsfx.controls;
-                            
+    requires htmlunit;
+    requires jdk.xml.dom;
+
     opens dev.neverrieght.kolodischi_transport to javafx.fxml;
     exports dev.neverrieght.kolodischi_transport;
 }
